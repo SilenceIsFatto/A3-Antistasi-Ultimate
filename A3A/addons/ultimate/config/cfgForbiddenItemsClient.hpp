b53b0a61d08555c78d913e6e0f7e088a154e2e28
MaxxLite <162753200+UAC-MaxxLite@users.noreply.github.com>
2024-07-19 09:11:40 +0300

    blacklist update

---
+++ A3A/addons/ultimate/config/cfgForbiddenItemsClient.hpp
@@ -0,0 +1,11 @@
+class SSV_unlimited_base : forbidden_unlimited_base 
+{
+    addons[] = {"auc_core"};
+};
+
+class SSV_limited_base : forbidden_limited_base 
+{
+    addons[] = {"auc_core"};
+};
+
+class UAC_Pistol_45 : SSV_unlimited_base {};
