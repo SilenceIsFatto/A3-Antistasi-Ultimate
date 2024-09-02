69a15dac2ac2f1e7548d8f604b1e1ceabd7ff3f6
traditional <katzepvp@gmail.com>
2024-09-02 18:38:47 +0200

    Patch 2

--- A3A/addons/maps/Antistasi_fapovo.fapovo/mapInfo.hpp
+++ A3A/addons/maps/Antistasi_fapovo.fapovo/mapInfo.hpp
@@ -42,14 +42,20 @@ class fapovo {
 	};
 	disabledTowns[] = {"graltech", "oilterminal", "owl", "coyote", "forge", "rangetiren", "sundial", "aris", "ufocamp", "gypsy", "radar2", "kalina", "bosco", "laguna", "sbp1", "sbp2"}; //no towns that need to be disabled
 	antennas[] = {
-		{1545.13,1279.5,0},{4480.38,629.25,0},{571.25,8023.13,0}
+		{1545.13,1279.5,0}, // ID: 1 - Upper Jabovo Outpost
+		{4480.38,629.25,0}, // ID: 2 - Airport Ivanograd
+		{571.25,8023.13,0}, // ID: 3 - Kruger Lab Outpost
+		{5665.13,3989.38,0}, // ID: 4 - Castle Serm
+		{467.452,4498.5,0}, // ID: 5 - MOB Mermaid
+		{8315.94,8295.46,0}, // ID: 6 - Airport Botana Intl.
+
 	};
 	antennasBlacklistIndex[] = {};
 	banks[] = {
 		
 	};
 	garrison[] = {
-		{},{},{},{}
+		{},{},{"airport_3","outpost_3","milbase_2","seaport_4","outpost_1","resource_8"},{}
 	};
 	fuelStationTypes[] = {
 		"Land_FuelStation_01_pump_F","Land_FuelStation_02_pump_F","Land_FuelStation_03_pump_F","Land_fs_feed_F","Land_FuelStation_Feed_F","Land_FuelStation_01_pump_malevil_F","Land_Fuelstation","Land_Fuelstation_army","Land_A_FuelStation_Feed","Land_Ind_FuelStation_Feed_EP1","Land_FuelStation_Feed_PMC"
@@ -59,14 +65,9 @@ class fapovo {
 	};
 	climate = "arid";
 	buildObjects[] = {
-		{"Land_fortified_nest_big_EP1", 300}, {"Land_Fort_Watchtower_EP1", 300}, {"Fortress2", 200}, {"Fortress1", 100}, {"Fort_Nest", 60},
-		{"Land_Shed_09_F", 120}, {"Land_Shed_10_F", 140}, {"ShedBig", 100}, {"Shed", 100}, {"ShedSmall", 60}, {"Land_GuardShed", 30},
-		// CUP sandbag walls
-		{"Land_BagFenceLong", 10}, {"Land_BagFenceShort", 10}, {"Land_BagFenceRound", 10},        //{"Land_BagFenceEnd", 0, 5}, 
-		// Other CUP fences
-		{"Land_fort_artillery_nest_EP1", 200}, {"Land_fort_rampart_EP1", 50}, {"Fort_Barricade", 50}, {"Fence", 20}, {"FenceWood", 10}, {"FenceWoodPalet", 10}, 
-		// Non-camo vanilla stuff
-		{"Land_SandbagBarricade_01_half_F", 20}, {"Land_SlumWall_01_s_2m_F", 5}, {"Land_PillboxBunker_01_hex_F", 200},
-		{"Land_Barricade_01_4m_F", 30}, {"Land_GuardBox_01_brown_F", 80}, {"Land_Tyres_F", 10}
-	};
+		BUILDABLES_HISTORIC,
+		BUILDABLES_MODERN_SAND,
+		BUILDABLES_ARID,
+		BUILDABLES_CUP,
+		BUILDABLES_UNIVERSAL
 };
