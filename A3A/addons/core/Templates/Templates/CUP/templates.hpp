a1d38dab7c453c1f561d96fd42fd3c15fb9ce07c
stutpip123 <77023662+stutpip123@users.noreply.github.com>
2024-10-08 10:10:40 +0100

    Update templates.hpp

--- A3A/addons/core/Templates/Templates/CUP/templates.hpp
+++ A3A/addons/core/Templates/Templates/CUP/templates.hpp
@@ -263,6 +263,7 @@
     class HAFM_Base : CUP_Base
     {
         requiredAddons[] = {"CUP_Creatures_People_Civil_Russia", "CUP_BaseConfigs", "CUP_AirVehicles_Core", "HAFM_Acc"};        // units, weapons, vehicles
+	};
     //CUP NorAF
     class NorAF_Base : CUP_Base
     {
