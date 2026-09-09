8435ef86ea4f3727ddc49e25a5349e04be9af600
Oliver Schieche <post@oliver-schieche.de>
2026-09-09 16:06:34 +0200

    Update for new plugin architecture

--- A3A/addons/ultimate/plugins/tracklist_editor/script_component.hpp
+++ A3A/addons/ultimate/plugins/tracklist_editor/script_component.hpp
@@ -1,6 +1,5 @@
-#define COMPONENT ultimate
 #define SUBCOMPONENT tracklist_editor
-#include "\x\A3A\addons\core\Includes\script_mod.hpp"
+#include "..\plugin.hpp"
 
 // Remember last X tracks so they don't get played again immediately
 #define REMEMBER_TRACKS 2
