4b6c8e08903da646bcfee64bbbce18505e280e24
Oliver Schieche <post@oliver-schieche.de>
2026-09-10 18:37:37 +0200

    Add element customization to GUI editor serialized output

--- A3A/addons/gui/dialogues/extendersWarningDialog.hpp
+++ A3A/addons/gui/dialogues/extendersWarningDialog.hpp
@@ -13,17 +13,17 @@ class GVAR(RscText): RscText {
 	UI_FONT_DEFAULT;
 };
 
-/* #Lecuwa
+/* #Guhuca
 $[
 	1.063,
 	["antistasi_extenders_warning_dialog",[[0,0,1,1],0.025,0.04,"GUI_GRID"],2,1,1],
-	[-1000,"StaticTitle",[1,"ANTISTASI ULTIMATE SETUP - EXTENDERS COMPATIBILITY NOTICE",["0.257656 * safezoneW + safezoneX","0.192 * safezoneH + safezoneY","0.500156 * safezoneW","0.033 * safezoneH"],[-1,-1,-1,-1],[0,0.8,0,1],[-1,-1,-1,-1],"","-1"],["moving = 1;"]],
+	[-1000,"StaticTitle",[1,"ANTISTASI ULTIMATE SETUP - EXTENDERS COMPATIBILITY NOTICE",["0.257656 * safezoneW + safezoneX","0.192 * safezoneH + safezoneY","0.500156 * safezoneW","0.033 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],["moving = 1;"]],
 	[-1002,"StaticBackground",[1,"",["0.257656 * safezoneW + safezoneX","0.225 * safezoneH + safezoneY","0.500156 * safezoneW","0.55 * safezoneH"],[-1,-1,-1,-1],[0,0,0,0.8],[-1,-1,-1,-1],"","-1"],[]],
-	[1001,"StaticTextWarning",[1,"WARNING: You have loaded extenders. Some of them failed the compatibility check to this version of Antistasi Ultimate.",["0.262812 * safezoneW + safezoneX","0.236 * safezoneH + safezoneY","0.489844 * safezoneW","0.11 * safezoneH"],[0.8,0,0,1],[0,0,0,1],[-1,-1,-1,-1],"","-1"],["style = QUOTE(ST_CENTER + ST_MULTI);"]],
-	[1500,"ListExtenders",[1,"",["0.262812 * safezoneW + safezoneX","0.357 * safezoneH + safezoneY","0.489844 * safezoneW","0.264 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],["type = CT_LISTNBOX;","idcLeft = -1;","idcRight = -1;","drawSideArrows = 0;"]],
+	[1001,"StaticTextWarning",[1,"WARNING: You have loaded extenders. Some of them failed the compatibility check to this version of Antistasi Ultimate.",["0.262812 * safezoneW + safezoneX","0.236 * safezoneH + safezoneY","0.489844 * safezoneW","0.11 * safezoneH"],[0.8,0,0,1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],["style = QUOTE(ST_CENTER + ST_MULTI);"]],
+	[1500,"ListExtenders",[1,"",["0.262812 * safezoneW + safezoneX","0.357 * safezoneH + safezoneY","0.489844 * safezoneW","0.264 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],["type = CT_LISTNBOX;","idcLeft = -1;","idcRight = -1;","drawSideArrows = 0;","UI_FONT_DEFAULT;"]],
 	[2800,"CheckConfirm",[1,"",["0.262812 * safezoneW + safezoneX","0.654 * safezoneH + safezoneY","0.0257812 * safezoneW","0.044 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
-	[1003,"LabelConfirm",[1,"I understand and I know what I am doing; let me continue setting up the game.",["0.288594 * safezoneW + safezoneX","0.654 * safezoneH + safezoneY","0.458906 * safezoneW","0.044 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
-	[1600,"BtnContinue",[1,"Continue",["0.45875 * safezoneW + safezoneX","0.709 * safezoneH + safezoneY","0.0825 * safezoneW","0.044 * safezoneH"],[-1,-1,-1,-1],[0,1,0,0.8],[-1,-1,-1,-1],"","-1"],[]]
+	[1600,"LabelConfirm",[1,"I understand and I know what I am doing; let me continue setting up the game.",["0.288594 * safezoneW + safezoneX","0.654 * safezoneH + safezoneY","0.458906 * safezoneW","0.044 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],["color[] = GUI_TEXT_COLOR;","colorActive[] = GUI_TEXT_COLOR;","colorBackground[] = {0, 0, 0, 0};","colorBackgroundActive[] = {0, 0, 0, 0};","colorBorder[] = {0, 0, 0, 0};","colorFocused[] = {0, 0, 0, 0};","colorShadow[] = {0, 0, 0, 0};","colorText[] = GUI_TEXT_COLOR;","style = QUOTE(ST_LEFT);","UI_FONT_DEFAULT;"]],
+	[1601,"BtnContinue",[1,"Continue",["0.45875 * safezoneW + safezoneX","0.709 * safezoneH + safezoneY","0.0825 * safezoneW","0.044 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]]
 ]
 */
 
@@ -42,7 +42,7 @@ class GVAR(ExtendersWarningDialog) {
 
     class Controls {
 		////////////////////////////////////////////////////////
-		// GUI EDITOR OUTPUT START (by goreSplatter, v1.063, #Mutybo)
+		// GUI EDITOR OUTPUT START (by goreSplatter, v1.063, #Guhuca)
 		////////////////////////////////////////////////////////
 
 		class StaticTitle: GVAR(headerBase)
@@ -83,13 +83,13 @@ class GVAR(ExtendersWarningDialog) {
 			idcLeft = -1;
 			idcRight = -1;
 			drawSideArrows = 0;
+			UI_FONT_DEFAULT;
 
 			idc = IDC_ANTISTASI_EXTENDERS_WARNING_DIALOG_LISTEXTENDERS;
 			x = QUOTE(0.262812 * safezoneW + safezoneX);
 			y = QUOTE(0.357 * safezoneH + safezoneY);
 			w = QUOTE(0.489844 * safezoneW);
 			h = QUOTE(0.264 * safezoneH);
-			UI_FONT_DEFAULT;
 		};
 		class CheckConfirm: RscCheckbox
 		{
@@ -101,7 +101,6 @@ class GVAR(ExtendersWarningDialog) {
 		};
 		class LabelConfirm: RscButton
 		{
-			idc = IDC_ANTISTASI_EXTENDERS_WARNING_DIALOG_LABELCONFIRM;
 			color[] = GUI_TEXT_COLOR;
 			colorActive[] = GUI_TEXT_COLOR;
 			colorBackground[] = {0, 0, 0, 0};
@@ -111,12 +110,14 @@ class GVAR(ExtendersWarningDialog) {
 			colorShadow[] = {0, 0, 0, 0};
 			colorText[] = GUI_TEXT_COLOR;
 			style = QUOTE(ST_LEFT);
+			UI_FONT_DEFAULT;
+
+			idc = IDC_ANTISTASI_EXTENDERS_WARNING_DIALOG_LABELCONFIRM;
 			text = QUOTE($STR_antistasi_extenders_warning_dialog_LabelConfirm);
 			x = QUOTE(0.288594 * safezoneW + safezoneX);
 			y = QUOTE(0.654 * safezoneH + safezoneY);
 			w = QUOTE(0.458906 * safezoneW);
 			h = QUOTE(0.044 * safezoneH);
-			UI_FONT_DEFAULT;
 		};
 		class BtnContinue: GVAR(ButtonBase)
 		{
