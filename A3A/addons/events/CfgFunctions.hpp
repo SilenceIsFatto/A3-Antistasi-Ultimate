4022d5b2c054a2b29973ce7eec4a33eefce66503
Oliver Schieche <post@oliver-schieche.de>
2026-09-11 17:16:16 +0200

    `removeAllEventListeners` never used

--- A3A/addons/events/CfgFunctions.hpp
+++ A3A/addons/events/CfgFunctions.hpp
@@ -3,7 +3,6 @@ class CfgFunctions {
 		class functions {
 			file = QPATHTOFOLDER(functions);
 			class addEventListener {};
-			class removeAllEventListeners {};
 			class removeEventListener {};
 			class triggerEvent {};
 			class validateEventArguments {};
